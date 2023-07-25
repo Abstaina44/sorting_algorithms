0bf0b00a22c67b4c403ac38bd91af8bde28aa285
Abstaina44 <padcliff88@gmail.com>
2023-07-25 10:37:38 -0700

    A function that sorts doubly linked List,ascending

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,30 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int get_pivot_index(int *array, size_t size, int first, int last);
+void sort_partition(int *array, int size, int first, int last);
+void swap(int **array, int first_index, int second_index);
+
+#endif
